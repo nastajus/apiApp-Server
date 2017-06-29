19bb753cf4df8bc9065df11ff5337f07c9267b2a
Ian Nastajus <ian.nastajus@gmail.com>
2017-06-28 21:27:16 -0400

    Date validation attempt, doesn't work, suspect applying prototypal inheritance incorrectly

--- index.js
+++ index.js
@@ -19,16 +19,22 @@ console.log ("Nastajus API task router is running.");
 
 
 function Auditable(createdDate, modifiedDate) {
-    this.createdDate = new Date().toLocaleString(); //createdDate;
-    this.modifiedDate = new Date().toLocaleString(); //modifiedDate;
+    this.createdDate = (createdDate)? createdDate.toLocaleString() : new Date().toLocaleString();
+    this.modifiedDate = (modifiedDate)? modifiedDate.toLocaleString() : new Date().toLocaleString();
+    if (!Object.prototype.toString.call(this.createdDate) === '[object Date]') {
+        throw new TypeError('createdDate is not a valid Date');
+    }
+    if (!Object.prototype.toString.call(this.modifiedDate) === '[object Date]') {
+        throw new TypeError('modifiedDate is not a valid Date');
+    }
 }
 
-function Task(category, text, type, tags) {
+function Task(category, text, type, tags, createdDate, modifiedDate) {
     this.category = category;
     this.text = text;
     this.type = type;
     this.tags = tags;
-    Auditable.call(this);
+    Auditable.call(this, createdDate, modifiedDate);
 
     Task.prototype.exportJson = function() {
         return JSON.stringify({ category: this.category, tags: this.tags, text: this.text, type: this.type, createdDate: this.createdDate, modifiedDate: this.modifiedDate});
@@ -72,8 +78,17 @@ function init() {
 }
 
 function createMockTasks() {
-    tasks.push(new Task("declutter", "complete nagging tasks", "habit"));
-    tasks.push(new Task("finance", "eliminate financial waste", "habit"));
+    let year = 2000;
+    let month = 11; //0-based
+    let date = 25;
+    let hours = 6;
+    let minutes = 30;
+    let seconds = 0;
+    let milliseconds = 0;
+    //native Date gets strinified as: "createdDate":"2000-12-25T11:30:00.000Z"
+    //toLocaleString() is pretty as:  "modifiedDate":"6/28/2017, 9:16:24 PM"
+    tasks.push(new Task("declutter", "complete nagging tasks", "habit", null, new Date(year, month, date, hours, minutes, seconds, milliseconds)));
+    tasks.push(new Task("finance", "eliminate financial waste", "habit", null, "not a date"));
     tasks.push(new Task("fun", "complete video games on my shame pile", "", ["fulfillment", "completionist"]));
     tasks.push(new Task("declutter", "physically eliminate fridge spoilage & clutter weekly", "habit"));
     tasks.push(new Task("", "complete execution of own projects", "", ["pride", "marketability", "fulfillment"]));
